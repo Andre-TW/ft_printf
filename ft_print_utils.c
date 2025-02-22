ca6f8f8d4b134aab8814e8145a72628611309fc3
Andre-TW <andregabriel020@gmail.com>
2025-02-22 17:28:34 +0100

    Add files via upload

--- ft_print_utils.c
+++ ft_print_utils.c
@@ -6,26 +6,34 @@
 /*   By: andsoare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 16:24:46 by andsoare          #+#    #+#             */
-/*   Updated: 2025/02/22 16:35:23 by andsoare         ###   ########.fr       */
+/*   Updated: 2025/02/22 17:27:50 by andsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printdigit(char c, int count)
+int ft_printchar(char c)
 {
+	int	count;
+
+	count = 0;
 	write(1, &c, 1);
 	count++;
-	return (count);
+	return(count);
 }
 
-int	ft_printstr(char *c, int count)
+int	ft_printstr(char *c)
 {
-	while (*c)
+	int	count;
+
+	count = 0;
+	while(*c != '\0')
 	{
 		write(1, c, 1);
 		c++;
 		count++;
 	}
-	return (count);
+	return(count);
 }
+
+int ft_printdigit
--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,39 @@
 /*   By: andsoare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 15:33:08 by andsoare          #+#    #+#             */
-/*   Updated: 2025/02/22 16:57:21 by andsoare         ###   ########.fr       */
+/*   Updated: 2025/02/22 17:28:15 by andsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	print_format(char spec, va_list ap)
+{
+	int count;
+
+	count = 0;
+	if (spec == 'c')
+		count += ft_printchar(va_arg(ap, int));
+	else if (spec == 's')
+		count += ft_printstr(va_arg(ap, char *));
+	else if (spec == 'd')
+		count += ft_printdigit(va_arg(ap, int), 10);
+	else if (spec == 'x')
+		count += printdigit(va_arg(ap, unsigned int), 16);
+	else if (spec == '%')
+		count += write(1, '%', 1);
+	else if (spec == 'X')
+		count += printdigitup(va_arg(ap, unsigned int), 16); //minha funcao vai ter as sinais mas em maiuscula, por isso up nao esqueca andre tw
+	else if (spec == 'i')
+		count += //tenho que criar ainda
+	else if (spec == 'p')
+		count += //void em *p eh hexa decimal, deus me abensoe nesse
+	else if (spec == 'u')
+		count += // printa um unsigned decimal sei la oq fi se vira
+	else
+		count += write(1, &spec,1);
+	return (count);
+}
 
 int	ft_printf(const char *arg, ...)
 {
