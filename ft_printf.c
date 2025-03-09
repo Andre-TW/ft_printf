7ea2dfd922522d79354dfb97c179d052c19ea979
Andre-TW <andregabriel020@gmail.com>
2025-03-09 16:27:38 +0100

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: andsoare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 15:33:08 by andsoare          #+#    #+#             */
-/*   Updated: 2025/03/04 16:32:03 by andsoare         ###   ########.fr       */
+/*   Updated: 2025/03/09 14:45:44 by andsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@ int	print_format(char spec, va_list ap)
 	else if (spec == 's')
 		return (ft_printstr(va_arg(ap, char *)));
 	else if (spec == 'x')
-		return (ft_printdigit((long)va_arg(ap, unsigned int), 16));
+		return (ft_printdigit(va_arg(ap, unsigned int), 16));
 	else if (spec == '%')
 		return (write(1, "%", 1));
 	else if (spec == 'X')
-		return (ft_printdigitup((long)va_arg(ap, unsigned int), 16));
+		return (ft_printdigitup(va_arg(ap, unsigned int), 16));
 	else if (spec == 'i' || spec == 'd')
-		return (ft_printdigit((long)va_arg(ap, int), 10));
+		return (ft_printdigit(va_arg(ap, int), 10));
 	else if (spec == 'p')
 		return (ft_printpointer(va_arg(ap, void *), 16));
 	else if (spec == 'u')
-		return (ft_printdigit((long)va_arg(ap, unsigned int), 10));
+		return (ft_printdigit(va_arg(ap, unsigned int), 10));
 	else
 		return (write (1, &spec, 1));
 }
@@ -45,8 +45,13 @@ int	ft_printf(const char *arg, ...)
 	{
 		if (*arg == '%')
 		{
-			count += print_format(*(++arg), ap);
-			++arg;
+			if ((*(++arg)) == '\0')
+				return (-1);
+			else
+			{
+				count += print_format(*arg, ap);
+				arg++;
+			}
 		}
 		else
 		{
